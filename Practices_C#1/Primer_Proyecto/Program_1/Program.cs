498917ccb8139d990d495a519861af6467ca5933
Chesnon <jeremyalexander135@gmail.com>
2026-02-10 19:18:28 -0400

    Subiendo Tarea 1

--- Practices_C#1/Primer_Proyecto/Program_1/Program.cs
+++ Practices_C#1/Primer_Proyecto/Program_1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Prueba de esta cosa para probar la prueba de probabilidad!");
+﻿class Program
+{
+    const double PI = 3.1416;
+
+    static void Main()
+    {
+        Console.WriteLine("PI es: " + PI);
+
+        PI = 23
+
+
+        Variables.Show();
+    }
+}
+
+class Variables
+{
+    public static void Show()
+    {
+        var number = 2;
+        var decimalValue = 0.56;
+        var name = "Papotico";
+        var booleana = false;
+        
+        Console.WriteLine("\n\n\n\n");
+        Console.WriteLine($"Variable numerica: {number}\nVariable decimal: {decimalValue}\nVariable String: {name}\nVariable booleana: {booleana}");
+    }
+}
---
+++ Practices_C#1/Prueba/Prueba/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Program.Fecha();
+        // TODO: agregar un case para cada clase
+    }
+
+
+    public static void Fecha()
+    {
+        //Obtener la hora del sistema
+        DateTime actual = DateTime.Now;
+        Console.WriteLine(actual);
+        /*y asi de facil
+          obtienes la hora
+         del sistema*/
+
+    }
+    public static void Float()
+    {
+        float DecimalGrande = 10152466.25f;
+        //byte NumeroBytes = 5 + DecimalGrande; ESTO DA ERROR
+
+        byte NumeroBytes = (byte)(5 + DecimalGrande); //gpt dice que se puede asi pero que el valor se "rompe"
+        Console.WriteLine(NumeroBytes);
+    }
+    public static void Operaciones()
+    {
+        int Entero = 20;
+        Console.WriteLine($"{Entero++}\n{Entero}");
+        Entero -= 5;
+        Console.WriteLine(Entero);
+
+        //operaciones
+        Console.WriteLine("Operaciones:\n Numeros iniciales:\n");
+
+        int IntNumberOne = 59;
+        int IntNumberTwo = 4;
+        int Result = 0;
+
+        Console.WriteLine($"Numero 1: {IntNumberOne}, Numero 2: {IntNumberTwo}\n");
+
+        Console.WriteLine("Operando:");
+
+        Result = ++IntNumberOne / IntNumberTwo--;
+        Console.WriteLine($"Resultado: {Result}");
+        Console.WriteLine($"Numero uno: {IntNumberOne}, Numero Dos: {IntNumberTwo}");
+    }
+}
